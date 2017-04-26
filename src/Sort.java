3c27e7159d920f15e79453c80b1e1b81f1122ab3
dominic massimo <dominicmassimo@dominics-iMac.fios-router.home>
2017-04-26 12:13:09 -0400

    Initial commit

---
+++ src/Sort.java
@@ -0,0 +1,20 @@
+/**
+ * Created by dominicmassimo on 4/26/17.
+ */
+public class Sort {
+    public static void swap(int oneNum, int otherNum, int [] numArr) {
+        int saveIdx = numArr[oneNum];
+        numArr[oneNum] = numArr[otherNum];
+        numArr[otherNum] = saveIdx;
+    }
+    public static void sort(int [] numArr) {
+        for(int i = 0; i < numArr.length; i++) {
+            for(int x = i; x > 0; x--) {
+                if(numArr[x] < numArr[x-1]) {
+                    Sort.swap(x, x-1, numArr);
+                }
+            }
+        }
+    }
+}
+
